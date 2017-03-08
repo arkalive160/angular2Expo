1ac34f6e17940c9c43713a1918d094b264706fd3
Cristian <cdcordobaa@unal.edu.co>
2017-03-08 03:23:24 +0000

    step 2: directives ngif ngfor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,22 +3,49 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-component',
   template: `
-              <h5>  {{title}}   </h5>
-              <h1> {{champion.name}} </h1>
-              <p> {{champion.class}} </p>
-              <p> {{champion.title}} </p>
+              <h1>  {{title}}   </h1>
+              
+              <div *ngFor="let champion of champions" >
+              
+                <h2> Name:  {{champion.name}} </h2>
+                <p>Class: {{champion.class}} </p>
+                <p>Title: {{champion.title}} </p>
+                <p *ngIf=" champion.mana > 0">
+                {{champion.name}} Shouts: {{champion.CombatPhrase}} !!!
+                </p>
+                
+                <p *ngIf= "champion.mana === 0">
+                {{champion.name}} Shouts: I need more mana :( !!
+                </p>
+                
+              
+              </div>
+              
             `
   
 })
 export class AppComponent {
   title = 'My Hero Stats';
   
-  champion = {
-    "id":  1,
-    "name": "Arka160",
-    "title": "The Angular Noble",
-    "class": "Magician"
-    
-  };
+  champions = [
+    {
+      "id":  1,
+      "name": "Arka160",
+      "title": "The Angular Noble",
+      "class": "Magician",
+      "CombatPhrase": "for the Grade!",
+      "healt": 80,
+      "mana": 150
+    },
+    {
+      "id":  2,
+      "name": "Neos",
+      "title": "The soul stealer",
+      "class": "Demon",
+      "CombatPhrase": "Everybody will lose >:D",
+      "healt": 300,
+      "mana": 0
+    }
+    ];
   
 }
